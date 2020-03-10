d39b1ee101293794d62410ebb571316676ee8bb8
hasanhuseyingelen <hasanhuseyingelen@gmail.com>
2020-03-10 16:22:12 +0300

    Create Emloyee Controller and View

---
+++ Paycompute/Controllers/EmployeeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Paycompute.Services;
+using Paycompute.Views;
+
+namespace Paycompute.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeService _employeeService;
+        public EmployeeController(IEmployeeService employeeService)
+        {
+            _employeeService = employeeService;
+        }
+        public IActionResult Index()
+        {
+            var employees = _employeeService.GetAll().Select(employee => new EmployeeIndexViewModel()
+            {
+                City = employee.City,
+                DateJoined = employee.DateJoined,
+                Designation = employee.Designation,
+                EmployeeNo = employee.EmployeeNo,
+                FullName = employee.FullName,
+                Gender = employee.Gender,
+                Id = employee.Id,
+                ImageUrl = employee.ImageUrl
+            }).ToList();
+            return View(employees);
+        }
+    }
+}
