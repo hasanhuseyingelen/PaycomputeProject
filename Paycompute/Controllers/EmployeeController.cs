70c99d8701492441be47b75b31fa95856c8ad6fa
hasanhuseyingelen <hasanhuseyingelen@gmail.com>
2020-03-13 09:38:47 +0300

    Add Employee Create View Model

--- Paycompute/Controllers/EmployeeController.cs
+++ Paycompute/Controllers/EmployeeController.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Paycompute.Models;
 using Paycompute.Services;
-using Paycompute.Views;
 
 namespace Paycompute.Controllers
 {
